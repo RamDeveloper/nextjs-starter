7c72212b66c7a74b5e90828a892c6ddba0e98f44
Ramkumar <ram1489@gmail.com>
2022-04-20 13:19:32 +0530

    Updated the coming soon text

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to NextJS Starter!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Awesome things are cooking :) <br>
+          Check back soon ;)
         </p>
       </main>
 
